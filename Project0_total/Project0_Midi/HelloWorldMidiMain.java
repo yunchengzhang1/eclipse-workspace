cea210b0434278c6fc905e8010c578921113efcc
[yunchengzhang1] <[yunchengz@smu.edu]>
2020-09-03 00:53:56 -0500

    Initial commit

---
+++ Project0_total/Project0_Midi/HelloWorldMidiMain.java
@@ -0,0 +1,100 @@
+/*
+ * c2017-2019 Courtney Brown 
+ * 
+ * Class: H
+ * Description: Demonstration of MIDI file manipulations, etc. & 'MelodyPlayer' sequencer
+ * 
+ */
+
+import processing.core.*;
+
+import java.util.*; 
+
+//importing the JMusic stuff
+import jm.music.data.*;
+import jm.JMC;
+import jm.util.*;
+import jm.midi.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.*;
+
+//import javax.sound.midi.*;
+
+			//make sure this class name matches your file name, if not fix.
+public class HelloWorldMidiMain extends PApplet {
+
+	MelodyPlayer player; //play a midi sequence
+	MidiFileToNotes midiNotes; //read a midi file
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("HelloWorldMidiMain"); //change this to match above class & file name 
+
+	}
+
+	//setting the window size to 300x300
+	public void settings() {
+		size(300, 300);
+
+	}
+
+	//doing all the setup stuff
+	public void setup() {
+		fill(120, 50, 240);
+
+		// returns a url
+		String filePath = getPath("mid/gardel_por.mid");
+		// playMidiFile(filePath);
+
+		midiNotes = new MidiFileToNotes(filePath); //creates a new MidiFileToNotes -- reminder -- ALL objects in Java must 
+													//be created with "new". Note how every object is a pointer or reference. Every. single. one.
+
+
+//		// which line to read in --> this object only reads one line (or ie, voice or ie, one instrument)'s worth of data from the file
+		midiNotes.setWhichLine(0);
+
+		player = new MelodyPlayer(this, 100.0f);
+
+		player.setup();
+		player.setMelody(midiNotes.getPitchArray());
+		player.setRhythm(midiNotes.getRhythmArray());
+	}
+
+	public void draw() {
+		player.play(); //play each note in the sequence -- the player will determine whether is time for a note onset
+
+	}
+
+	//this finds the absolute path of a file
+	String getPath(String path) {
+
+		String filePath = "";
+		try {
+			filePath = URLDecoder.decode(getClass().getResource(path).getPath(), "UTF-8");
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return filePath;
+	}
+
+	//this function is not currently called. you may call this from setup() if you want to test
+	//this just plays the midi file -- all of it via your software synth. You will not use this function in upcoming projects
+	//but it could be a good debug tool.
+	void playMidiFile(String filename) {
+		Score theScore = new Score("Temporary score");
+		Read.midi(theScore, filename);
+		Play.midi(theScore);
+	}
+
+	//this starts & restarts the melody.
+	public void keyPressed() {
+		if (key == ' ') {
+			player.reset();
+			println("Melody started!");
+		}
+	}
+}
+
